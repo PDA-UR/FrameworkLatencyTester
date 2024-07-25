559a835c0da719bdd83bff7442f49a103a01c843
Andreas Schmid <andreas.schmid@ur.de>
2024-07-25 11:52:35 +0200

    update tearing detector to match new measuring setup

--- tearing_detector.py
+++ tearing_detector.py
@@ -1,15 +1,31 @@
 import numpy as np
 import cv2
 import os
+import json
 
-BORDER_TOP = int(os.environ['BORDER_TOP'])
-BORDER_BOTTOM = int(os.environ['BORDER_BOTTOM'])
-OFFSET = int(os.environ['OFFSET'])
-THRESHOLD = 50
+#BORDER_TOP = int(os.environ['BORDER_TOP'])
+#BORDER_BOTTOM = int(os.environ['BORDER_BOTTOM'])
+#OFFSET = int(os.environ['OFFSET'])
+THRESHOLD = -50
+
+with open('offsets.conf', 'r') as f:
+    offset_data = f.read()
+
+offsets = json.loads(offset_data)
+#print(offsets)
+
+OFFSET = int(offsets['OFFSET'])
+BORDER_TOP = int(offsets['BORDER_TOP'])
+BORDER_BOTTOM = int(offsets['BORDER_BOTTOM'])
 
 img = cv2.imread('screen_capture.png', cv2.IMREAD_GRAYSCALE)
+#img = cv2.imread('test_bright_screen.png', cv2.IMREAD_GRAYSCALE)
 
-line = img[OFFSET,BORDER_TOP:BORDER_BOTTOM]
+#print(img.shape)
+
+line = img[OFFSET,BORDER_BOTTOM:BORDER_TOP]
+
+#print(len(line))
 
 deriv = []
 
@@ -20,13 +36,16 @@ for x in range(len(line)):
     dx = int(line[x]) - line[x-1]
     deriv.append(dx)
 
+#print(deriv)
+
 #plt.plot(line)
 #plt.plot(deriv)
 #ax = plt.gca()
-position = np.argmax(deriv)
+position = np.argmin(deriv)
+#print('position', position)
 
-if deriv[position] > THRESHOLD:
+if deriv[position] < THRESHOLD:
     #plt.axvline(position, color='r', dashes=(1, 4))
-    print(position / len(deriv))
+    print(1 - (position / len(deriv)))
 else:
     print(0.0)
