6dd3f523b69d9ffe9926f5e90041c190151d07c8
Andreas Schmid <andreas.schmid@ur.de>
2025-11-03 15:27:11 +0100

    update camera control to support FLIR BFS120S4C 4k camera

--- run_camera.py
+++ run_camera.py
@@ -2,6 +2,7 @@ import time
 import PySpin
 import cv2
 import sys
+import shutil
 
 NUM_IMAGES = 0
 
@@ -14,6 +15,7 @@ if len(sys.argv) > 1:
         NUM_IMAGES = 0
 
 def acquire_images(cam, n):
+    print('acquire_images()')
     try:
         images = []
 
@@ -28,6 +30,12 @@ def acquire_images(cam, n):
                 continue
             #image.Save(f'img_{i}.png')
             image.Save(f'screen_capture.png')
+            if NUM_IMAGES != 0:
+                #print('save image')
+                #image_data = image.GetNDArray()
+                #cv2.imwrite(f'test_images_2/img_{counter}.png', image_data)
+                #image.Save(f'test_images_2/img_{counter}.png')
+                shutil.copyfile('screen_capture.png', f'test_images_2/img_{counter:03}.png')
             #image_data = image.GetNDArray()
             #cv2.imwrite(f'img_{i}.png', image_data)
 
@@ -37,11 +45,17 @@ def acquire_images(cam, n):
             counter += 1
 
             if NUM_IMAGES != 0:
+                #print('save image')
+                #image_data = image.GetNDArray()
+                #cv2.imwrite(f'test_images_2/img_{counter}.png', image_data)
+                #image.Save(f'test_images_2/img_{counter}.png')
                 if counter > NUM_IMAGES:
                     break
 
     except PySpin.SpinnakerException as ex:
         print('ERROR:', ex)
+    except Exception as ex:
+        print('Exception:', ex)
 
 def init_camera():
     system = PySpin.System.GetInstance()
@@ -58,13 +72,51 @@ def init_camera():
     cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
     #cam.ExposureTime.SetValue(float(100)) #250
     cam.GainAuto.SetValue(PySpin.GainAuto_Off)
-    cam.Gain.SetValue(6) #4
+    cam.Gain.SetValue(2) #4
+    #cam.SensorShutterMode.SetValue(PySpin.SensorShutterMode_Global)
+    #cam.TriggerDelay.SetValue(0)
+    #cam.TriggerMode.SetValue(PySpin.TriggerMode_On)
+    return cam, system
+
+def init_camera_4k():
+    system = PySpin.System.GetInstance()
+    cam = system.GetCameras()[0]
+    cam.Init()
+    #cam.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+    #cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)#SingleFrame)
+    cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_SingleFrame)
+    cam.ExposureMode.SetValue(PySpin.ExposureMode_TriggerWidth) # Timed
+    cam.TriggerSelector.SetValue(PySpin.TriggerSelector_FrameStart)
+    cam.TriggerSource.SetValue(PySpin.TriggerSource_Line3)
+    #cam.TriggerOverlap.SetValue(PySpin.TriggerOverlap_Off)
+    #cam.TriggerOverlap.SetValue(PySpin.TriggerOverlap_ReadOut)
+    cam.TriggerActivation.SetValue(PySpin.TriggerActivation_LevelLow)#FallingEdge
+    cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Off)
+    #cam.ExposureTime.SetValue(float(100)) #250
+    cam.GainAuto.SetValue(PySpin.GainAuto_Off)
+    cam.Gain.SetValue(12) #10
+    # switch camera to line mode
+    #cam.Height.SetValue(100)
+    #cam.OffsetY.SetValue(2600)
+    #cam.Height.SetValue(600)
+    #cam.OffsetY.SetValue(2300)
+    cam.OffsetY.SetValue(0)
+    cam.Height.SetValue(600)
+    #cam.PixelFormat.SetValue(PySpin.PixelFormat_Mono8)
+    cam.SensorShutterMode.SetValue(PySpin.SensorShutterMode_GlobalReset)
+    #cam.SensorShutterMode.SetValue(PySpin.SensorShutterMode_Rolling)
+    cam.PixelFormat.SetValue(PySpin.PixelFormat_BayerRG8)
+    #cam.PixelFormat.SetValue(PySpin.PixelFormat_RGB8)
     #cam.TriggerDelay.SetValue(0)
     #cam.TriggerMode.SetValue(PySpin.TriggerMode_On)
     return cam, system
 
 def main():
-    cam, system = init_camera()
+    #cam, system = init_camera()
+    cam, system = init_camera_4k()
+
+    acquisition_rate_hz = cam.AcquisitionLineRate.GetValue()
+    print('acquisition rate:', acquisition_rate_hz)
 
     acquire_images(cam, NUM_IMAGES)
 
--- tearing_detector.py
+++ tearing_detector.py
@@ -3,6 +3,8 @@ import cv2
 import os
 import json
 import sys
+import time
+from matplotlib import pyplot as plt
 
 #BORDER_TOP = int(os.environ['BORDER_TOP'])
 #BORDER_BOTTOM = int(os.environ['BORDER_BOTTOM'])
@@ -20,17 +22,34 @@ BORDER_TOP = int(offsets['BORDER_TOP'])
 BORDER_BOTTOM = int(offsets['BORDER_BOTTOM'])
 
 IMAGE_PATH = 'screen_capture.png'
+SHOW_PLOT = False
 
 if len(sys.argv) > 1:
     IMAGE_PATH = sys.argv[1]
+
+if len(sys.argv) > 2:
+    try:
+        SHOW_PLOT = bool(sys.argv[2])
+    except:
+        pass
+
+# HACK: wait a bit so the file is saved for sure
+# failed a few times without the wait in debug mode (run_camera.py saving the image twice)
+time.sleep(0.1)
 
 img = cv2.imread(IMAGE_PATH, cv2.IMREAD_GRAYSCALE)
 #img = cv2.imread('test_bright_screen.png', cv2.IMREAD_GRAYSCALE)
 
 #print(img.shape)
 
-line = img[OFFSET,BORDER_BOTTOM:BORDER_TOP]
-line = np.flip(line)
+## set offset to 0 if camera is in line scan mode
+#OFFSET = 0
+#line = img[OFFSET,BORDER_BOTTOM:BORDER_TOP]
+
+# sum up multiple lines to increase dynamic range and reduce influence of noise and moiree
+line = np.sum(img[OFFSET:OFFSET+5,BORDER_BOTTOM:BORDER_TOP], axis=0)
+#print(len(line))
+#line = np.flip(line)
 
 #print(len(line))
 
@@ -49,12 +68,30 @@ for x in range(len(line)):
 #plt.plot(deriv)
 #ax = plt.gca()
 
+
+# if left side of the monitor is on top of the camera frame:
+# use argmin() on derivative and invert result (1 - (pos / len))
+# because the white part of the teared screen is on the left hand side
+# therefore we need to find a drop in brightness from white to black
+# alternative would be to invert the image
+
 #print(np.argmax(deriv), np.argmin(deriv))
-position = np.argmax(deriv)
+#position = np.argmax(deriv)
+position = np.argmin(deriv)
 #print('position', position)
 
-if deriv[position] > THRESHOLD:
+if abs(deriv[position]) > THRESHOLD:
     #plt.axvline(position, color='r', dashes=(1, 4))
-    print((position / len(deriv)))
+    # position is percent from top to bottom, therefore we have to invert it
+    print(1 - (position / len(deriv)))
 else:
     print(0.0)
+
+if SHOW_PLOT:
+    plt.plot(line)
+    plt.plot(deriv)
+    plt.imshow(img[:,BORDER_BOTTOM:BORDER_TOP])
+    ax = plt.gca()
+    ax.axvline(position, color='black', dashes=(2, 4))
+    ax.set_ylim(img.shape[0], -200)
+    plt.show()
