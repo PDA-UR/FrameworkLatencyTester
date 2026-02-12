430317cc813e52972ca0a12a3025930dc3cb0f81
Andreas Schmid <andreas.schmid@ur.de>
2026-02-12 16:00:08 +0100

    update sensor position detector: everything runs automatically now and data is stored in csv

--- sweep_sensor_positions.py
+++ sweep_sensor_positions.py
@@ -4,8 +4,10 @@ import pygame
 import sys
 import time
 import serial
+import numpy as np
+import pandas as pd
 
-ser = serial.Serial('/dev/ttyACM0')  # open serial port
+ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 ser.flushInput()
 
 time.sleep(1)
@@ -21,22 +23,42 @@ screen.fill(background_color_black)
 
 pygame.display.flip()
 
-line_width = 3
+line_width = 1
 
-for y in range(height):
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        running = False
-    if not running:
-        break
-    
-    screen.fill(background_color_black)
-    pygame.draw.line(screen, color_white, (0, y), (width, y), line_width)
-    
-    pygame.display.update()
 
-    time.sleep(0.02)
-    ser.write('b'.encode())
-    ser_bytes = ser.readline()
-    decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
-    print(decoded_bytes)
+y_values = list(range(0, 80)) + list(range(500, 580)) + list(range(1000, 1080))
+
+lines = []
+
+#for y in range(height):
+#for y in np.arange(0, height, 10):
+for line_width in [1, 3, 5]:
+    for y in y_values:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            running = False
+        if not running:
+            break
+        
+        screen.fill(background_color_black)
+        pygame.draw.line(screen, color_white, (0, y), (width, y), line_width)
+        
+        pygame.display.update()
+
+        time.sleep(0.02)
+        ser.write('b'.encode())
+        ser_bytes = ser.readline()
+        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")
+        substrings = decoded_bytes.split(',')
+        top = int(substrings[0][3:])
+        center = int(substrings[1][3:])
+        bottom = int(substrings[2][3:])
+        print(y, top, center, bottom)
+        #print(decoded_bytes)
+        line = {'y' : y, 'linewidth' : line_width, 'top' : top, 'center' : center, 'bottom' : bottom}
+        lines.append(line)
+
+ser.close()
+
+df = pd.DataFrame(lines)
+df.to_csv('sensor_positions.csv')
