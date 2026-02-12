62a227f80027edfd992d377121b15fb693ddcbe9
Andreas Schmid <andreas.schmid@ur.de>
2026-02-12 15:54:38 +0100

    latency tester: use cerr for debug prints

--- latency_tester_parport/camera.cpp
+++ latency_tester_parport/camera.cpp
@@ -15,7 +15,17 @@ double CameraHandler::runTearingDetection()
 {
     // todo: add exception handling
     //cout << "tearing detection" << endl;
+    //cerr << "before tearing detector" << endl;
     string result_string = exec("su latency -c 'python3.10 tearing_detector.py'");
+    //cerr << "after tearing detector" << endl;
     //cerr << "tearing:" << result_string << endl;
     return stod(result_string);
+}
+
+void CameraHandler::storeTearingData(int id)
+{
+	//cerr << "before mv command" << endl;
+	string command = "su latency -c 'mv tearing_capture_line.png tearing_img_tmp/" + to_string(id) + ".png'";
+	//cerr << "after mv command" << endl;
+	exec(command.c_str());
 }
